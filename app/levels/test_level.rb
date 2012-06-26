6e06bff3f8d08993afc20207d93ba717753cf0b5
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-06-26 02:20:45 -0400

    added another enemy type to test level

--- app/levels/test_level.rb
+++ app/levels/test_level.rb
@@ -6,6 +6,9 @@ class TestLevel
   def update time
     @delagate.create_enemies if time == 0
     @delagate.create_enemies if time % 120 == 0
+
+    @delagate.create_enemy Point.new(0, 400), Path.new(Array.new(340, [1, 0])) if time % 160 == 0
+    @delagate.create_enemy Point.new(300, 400), Path.new(Array.new(340, [-1, 0])) if time % 160 == 0
   end
   
 end
