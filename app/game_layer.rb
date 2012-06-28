5ddee32422748824a5d04db08f9e0043e47cec92
Jacob O'Donnell <jacobodonnell@gmail.com>
2012-06-28 19:45:52 -0400

    whopps meant 250

--- app/game_layer.rb
+++ app/game_layer.rb
@@ -24,7 +24,7 @@ class GameLayer < CCLayer
     bullets_batch = CCSpriteBatchNode.batchNodeWithFile("sprites.png")
     addChild(bullets_batch, z:1, tag:0)
 
-    @bullets = 350.times.collect do |x|
+    @bullets = 250.times.collect do |x|
       bullet = CCSprite.spriteWithSpriteFrameName("bullet.png")
       bullet.setPosition(CGPoint.new(rand(320), rand(400)))
       bullets_batch.addChild bullet, z: 1, tag:x
@@ -36,7 +36,7 @@ class GameLayer < CCLayer
 
   def update
     node = getChildByTag 0
-    350.times {|num|
+    250.times {|num|
       bullet = node.getChildByTag num
       if bullet.position.y > 500
         bullet.setPosition(CGPoint.new(bullet.position.x, 0))
